154d3d12275073a810d0e1de610c5d264fbb2329
Jonathan Willian <jonathan.willian.tod@gmail.com>
2018-05-07 19:08:23 -0300

    Added an localstorage persistence to the redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { save, load } from 'redux-localstorage-simple';
 
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
@@ -11,7 +12,7 @@ import registerServiceWorker from './registerServiceWorker';
 import reducers from './rootReducer';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = applyMiddleware()(createStore)(reducers, devTools);
+const store = applyMiddleware(save())(createStore)(reducers, load(), devTools);
 
 ReactDOM.render(
   <Provider store={store}>
